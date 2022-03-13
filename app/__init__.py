c0fa60d75843b3ccbd5eb583b4a8666fb6115610
John-Kimani <kimwaweru100@gmail.com>
2022-03-13 11:38:12 +0300

    Fix: Initialize bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,18 @@
 from flask import Flask
 from config import Config
+from flask_bootstrap import Bootstrap
 
 
-
+bootstrap = Bootstrap()
 
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
 
+    '''
+    Initialize requirements
+    '''
+    bootstrap.init_app(app)
 
     '''
     Registering main blueprint
