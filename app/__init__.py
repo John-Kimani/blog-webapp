83a0c91eb0755d81971837ff0d48ad36ab0b91f0
John-Kimani <kimwaweru100@gmail.com>
2022-03-14 01:02:40 +0300

    Dev: Set authenticate url prefix

--- app/__init__.py
+++ app/__init__.py
@@ -32,7 +32,7 @@ def create_app(config_class=Config):
     Registering auth blueprint
     '''
     from app.auth import auth as auth_bp
-    app.register_blueprint(auth_bp)
+    app.register_blueprint(auth_bp, url_prefix= '/authenticate')
 
 
 
