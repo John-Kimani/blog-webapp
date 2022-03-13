f856633ec08e1a3b9fb98b5726c4693936a045b6
John-Kimani <kimwaweru100@gmail.com>
2022-03-13 15:34:25 +0300

    Dev: Creates login form

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,7 @@
 from flask import render_template, redirect,url_for
 from app.main import main
 from app.request import get_random_qoutes
+from app.main.forms import LoginForm
 
 
 
@@ -14,3 +15,12 @@ def index():
     random_quote = get_random_qoutes()
 
     return render_template('index.html', title=title, quotes=random_quote)
+
+@main.route('/login')
+def login():
+    '''
+    View funciton that handles users log in
+    '''
+    form = LoginForm()
+    title = 'LogIn'
+    return render_template('auth/login.html', title=title, form=form)
--- config.py
+++ config.py
@@ -6,3 +6,4 @@ class Config(object):
     General configuration class
     '''
     # RANDOM_QOUTES_URL = os.environ.get('QUOTES_URL')
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
