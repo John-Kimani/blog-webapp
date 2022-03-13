a2f58eb2e60298c2f0deeb2f711a737eaafccab6
John-Kimani <kimwaweru100@gmail.com>
2022-03-13 11:29:07 +0300

    Register Main blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -9,5 +9,12 @@ def create_app(config_class=Config):
     app.config.from_object(config_class)
 
 
+    '''
+    Registering main blueprint
+    '''
+    from app.main import main as main_bp
+    app.register_blueprint(main_bp)
+
+
 
     return app
